7928f6d3dfd52c94cdd09ed50216c7d4255e82f9
Gaws21 <47149231+Gaws21@users.noreply.github.com>
2020-11-29 19:12:10 -0300

    Update create_external_table.py

--- create_external_table.py
+++ create_external_table.py
@@ -10,37 +10,27 @@ from google.cloud import bigquery
 
     Passe o caminho de sua chave json como parâmetro para o método from_service_account_json
 """
-client = bigquery.Client.from_service_account_json("/home/asus/Documentos/ADS/2020/venv_bq_py3.7/venv/key_service_account_create_1.json")
+client = bigquery.Client.from_service_account_json("your_keys_path.json")
 
 """
     Instanciar um objeto ExternalConfig do tipo CSV.
-    Por default, os objetos ExternalConfig são CSV.
+    Por default os objetos ExternalConfig são CSV.
     
     Seguindo o mandamentos do Zen Python:
       Explícito é melhor que implícito.
 """
-table_ext_clear = bigquery.external_config.ExternalConfig("CSV")
+table_ext_origem = bigquery.external_config.ExternalConfig("CSV")
 
 """
     Por padrão, arquivos csv são delimitados por ponto e virgula.
     Porém é comum se deparar com arquivos com extensão .csv com outros tipos de delimitadores.
     Em especial, arquivos exportados do Excel para csv são automaticamente atribuídos ponto e virgula
     como o tipo de delimitador. 
-    Caso não for configurado outro tipo específico pelo usuário, virgula será atribuído por defaut.
+    Caso não for configurado outro tipo específico pelo usuário, virgula será atribuído por defaut para 
+    o método field_delimiter.
 """
-#setar o atributo field_delimiter do arquivo externo
-table_ext_clear.options.field_delimiter = ";"
-
-"""
-    Esse atributo permite que ao se executar a consulta, caso existe linhas com problemas,  
-    então serão ignoradas. 
-    No caso do arquivo utilizado, existiam algumas linhas que geravam erro ao executar uma consulta.
-    LEMBRA-SE:Ignorar linhas é perder registros. O melhor a se fazer é corrigir seus dados externos
-    na origem.
-"""
-
-#setar um valor baixo de preferência. 
-table_ext_clear.max_bad_records = 10
+#setar o atributo field_delimiter com o delimitador do arquivo externo
+table_ext_origem.options.field_delimiter = ";"
 
 """
     No nosso exemplo, por questões didáticas, estamos utilizando o Google Drive.
@@ -49,30 +39,35 @@ table_ext_clear.max_bad_records = 10
 """
 
 #setar a uri onde seus dados estão armazenados.
-table_ext_clear.source_uris = "https://drive.google.com/open?id=1qBntr4-o7G3nY4_OomLoNdA7EUrV00nG"
+table_ext_clear.source_uris = "your_uri"
 
 """
-     Existem diferentes maneiras de expecificar um schema.
-     No nosso exemplo utilizados o schema da nossa primeira tabela que apresentou problemas
-     em certas linhas.
+     Existem diferentes maneiras de especificar um schema.
+     Nesse caso foi utilizado o método SchemaField para especificar nosso esquema.
 """
 
-tb_ext_origem = client.get_table("dataset1-261923.set1.table_ext_origem")
+#configurar o schema
+schema = [
+    bigquery.SchemaField("data", "STRING", mode="NULLABLE"),
+    bigquery.SchemaField("descricao", "STRING", mode="NULLABLE"),
+    bigquery.SchemaField("valor", "STRING", mode="NULLABLE"),
+    bigquery.SchemaField("quantidade", "STRING", mode="NULLABLE"),
+]
 
-schema = tb_ext_origem.schema
 
-table_ext_clear.schema = schema
+#objeto que representa a tabela a ser criada recebe o schema
+table_ext_origem.schema = schema
 
 #criar o id de destino da sua tabela. Composto por: nome_projeto.nome_dataset.nome_tabela
-table_id = "dataset1-261923.set1.table_ext_clear"
+table_id = "projeto.dataset.table_ext_origem"
 
 #instanciar um objeto do tipo Table, inicializado com o id da tabela de destino.
 table = bigquery.Table(table_id)
 
 #client.delete_table(table)
 
-#setar o atributo external_data_configuration com o objeto ExternalConfig criado na linha 23.
-table.external_data_configuration = table_ext_clear
+#setar o atributo external_data_configuration com o objeto ExternalConfig criado na linha 22.
+table.external_data_configuration = table_ext_origem
 
 #utlizar o método create_table passando como parâmetro o objeto table.
 table_created = client.create_table(table)  # Make an API request.
